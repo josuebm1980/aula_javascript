435dedffff6ed670f32df57fc7198378b56b2849
Josué Moraes <quijobem@hotmail.com>
2020-06-20 19:32:36 -0300

    aulas javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 
-
-
-
-
-
-
 var count;
 for(count = 0; count <= 5; count++){
     alert(count);
 };
-
-
-
-
 
 /* LAÇO DE REPSTIÇÃO
 var count = 0;
@@ -35,17 +25,3 @@ if(idade>=18){
 //var lista = ["maça", "pera", "laranja"];
 //alert(lista[3]);
 //alert.push["uva"];
-
-
-
-//var nome = "Josué Moraes";
-//var idade = 29;
-//var idade2 = 10;
-//var frase = "Japão é o melhor time do mundo";
-//alert(nome + " tem " + idade + " anos"); concatenação;
-//alert(idade + idade2);
-//console.log(nome);
-//console.log(idade + idade2);
-//alert(frase.replace("Japão", "Brasil")); substitui o 'Japão' pelo 'Brasil' na frase
-//alert(frase.toUpperCase()); coloca tudo para maiuscula;
-//alert(frase.toLowerCase()); coloca tudo minusculo;
