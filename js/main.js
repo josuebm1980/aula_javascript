900fb0dde467e03b1fb5f88ca74b1ab8a4e1d9a2
Josué Moraes <quijobem@hotmail.com>
2020-06-20 18:38:22 -0300

    javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
-var nome = "Josué Moraes";
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
+
+
+
+
+
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
+/*
+
+
+
+
+/* LAÇO DE REPSTIÇÃO
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+/*
+
+/* CONDICIONAIS
+var idade = prompt("qual é sua idade"); no comando prompt fazer uma pergunta no browser do usário guardando a respota na variável idade;
+if(idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+/*
+
+//var lista = ["maça", "pera", "laranja"];
+//alert(lista[3]);
+//alert.push["uva"];
+
+
+
+//var nome = "Josué Moraes";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos"); concatenação;
 //alert(idade + idade2);
 //console.log(nome);
