b7654c09badba8a91d300d5133f0f9fa57789d29
Josué Moraes <quijobem@hotmail.com>
2020-06-20 15:16:42 -0300

    concatenação replace touppercase tolowercase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("meu primeiro js");
+var nome = "Josué Moraes";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos"); concatenação;
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//alert(frase.replace("Japão", "Brasil")); substitui o 'Japão' pelo 'Brasil' na frase
+//alert(frase.toUpperCase()); coloca tudo para maiuscula;
+//alert(frase.toLowerCase()); coloca tudo minusculo;
