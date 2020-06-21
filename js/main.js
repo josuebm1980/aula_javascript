05741ed93e89f130c4003b3209a806f9cbfcffb5
Josué Moraes <quijobem@hotmail.com>
2020-06-20 22:39:01 -0300

    functions em javascript

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
-
-var count;
-for(count = 0; count <= 5; count++){
-    alert(count);
-};
-
-/* LAÇO DE REPSTIÇÃO
-var count = 0;
-while(count <= 5){
-    console.log(count);
-    alert(count);
-    count++;
+function clicou(){
+    alert("Obrigado por clicar");
 }
-/*
 
-/* CONDICIONAIS
-var idade = prompt("qual é sua idade"); no comando prompt fazer uma pergunta no browser do usário guardando a respota na variável idade;
-if(idade>=18){
-    alert("maior de idade");
-}else{
-    alert("menor de idade");
+
+
+/*function soma (n1, n2){
+      return n1 + n2;
+
 }
+alert(soma(1, 10));
+*/
 /*
-
-//var lista = ["maça", "pera", "laranja"];
-//alert(lista[3]);
-//alert.push["uva"];
+var count = 0
+for(count = 0; count <=5; count++){
+    alert(count);
+    count = count + 1;
+}
+*/
